55d9fedd1558d9147f511e27070f5db76583e106
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-23 12:21:46 +0600

    newly added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function add(a,b){
-    return a + b;
+// function add(a,b){
+//     return a + b;
+// }
+
+
+// console.log(add(2,3));
+
+
+
+function substract(a,b){
+    return a - b;
 }
 
-
-console.log(add(2,3));
+console.log(substract(5,2))
